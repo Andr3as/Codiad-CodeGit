d86d254692f149e221b7916014fc363e3e8edeb6
inpos <inpos@yandex.ru>
2017-02-08 15:19:38 +0300

    Вывод ошибки комита

--- class.git.php
+++ class.git.php
@@ -83,6 +83,7 @@
             if (!is_dir($path)) return false;
             chdir($path);
             if ($this->setGitSettings($path)) {
+            	error_log($msg);
                 $result = $this->executeCommand("git commit -m \"" . $msg . "\"");
                 return $this->parseShellResult($result, "Changes commited", "Failed to commit changes!");
             }
